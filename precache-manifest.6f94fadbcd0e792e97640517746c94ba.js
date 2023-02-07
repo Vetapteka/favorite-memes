4c94d80db5deaaef86543f756bafab74e278e6b4
vetapteka <cvetka_top@mail.ru>
2023-02-07 14:16:03 +0300

    (true true) fetching works correctlly

---
+++ precache-manifest.6f94fadbcd0e792e97640517746c94ba.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "5a5bf18b24d52e146bd3",
+    "url": "/favorite-memes/static/js/main.5a5bf18b.chunk.js"
+  },
+  {
+    "revision": "d8679607e902f36e89ee",
+    "url": "/favorite-memes/static/js/1.d8679607.chunk.js"
+  },
+  {
+    "revision": "35678c3559b381bdda04",
+    "url": "/favorite-memes/static/js/runtime~main.35678c35.js"
+  },
+  {
+    "revision": "4f2195f382a339c7b9d0b854978bdc53",
+    "url": "/favorite-memes/index.html"
+  }
+];
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.345b008c35e68c28f4808421de9dd83b.js"
+  "/favorite-memes/precache-manifest.6f94fadbcd0e792e97640517746c94ba.js"
 );
 
 workbox.clientsClaim();
@@ -28,7 +28,7 @@ self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.suppressWarnings();
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute("/index.html", {
+workbox.routing.registerNavigationRoute("/favorite-memes/index.html", {
   
   blacklist: [/^\/_/,/\/[^/]+\.[^/]+$/],
 });
