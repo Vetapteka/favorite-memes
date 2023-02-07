d80c32a4881d9ce54e88358378afe86fc358185a
vetapteka <cvetka_top@mail.ru>
2023-02-07 12:40:29 +0300

    Updates

---
+++ precache-manifest.35327369ac186d0657cb897e8f2d918f.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = [
+  {
+    "revision": "d7a849f702057b06c874",
+    "url": "/favorite-memes/static/js/main.d7a849f7.chunk.js"
+  },
+  {
+    "revision": "d8679607e902f36e89ee",
+    "url": "/favorite-memes/static/js/1.d8679607.chunk.js"
+  },
+  {
+    "revision": "35678c3559b381bdda04",
+    "url": "/favorite-memes/static/js/runtime~main.35678c35.js"
+  },
+  {
+    "revision": "1796c2d4bd1424efed93a56998eec063",
+    "url": "/favorite-memes/index.html"
+  }
+];
